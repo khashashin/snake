b167135f2148858ca742b8e21c34e2191cde1d2d
khashashin <khashashin@gmail.com>
2017-09-16 19:46:20 +0200

    created snake

--- src/core/snake/SnakeGame.java
+++ src/core/snake/SnakeGame.java
@@ -2,31 +2,49 @@ package core.snake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import snake.objaects.Snake;
 
 @SuppressWarnings("serial")
-public class SnakeGame extends JPanel{
-	
+public class SnakeGame extends JPanel implements ActionListener{
+// one step size
 	public static final int SCALE = 32;
+// the size of one square
 	public static final int WIDTH = 20;
 	public static final int HEIGHT = 20;
+	public static final int SPEED = 5;
+
+	Snake s = new Snake(10, 10, 9, 10);
+	Timer t = new Timer(1000/SPEED, this);
 	
 	public SnakeGame() {
-		
+		t.start();
 	}
 	
 	public void paint(Graphics g) {
+// color of the playing field
 		g.setColor(color(5, 50, 10));
 		g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+// lines color
 		g.setColor(color(255, 216, 0));
-		
+// number of lines on the scale x
 		for(int xx = 0; xx <= WIDTH*SCALE; xx+=SCALE) {
 			g.drawLine(xx, 0, xx, HEIGHT*SCALE);
 		}
+// number of lines on the scale y
 		for(int yy = 0; yy <= WIDTH*SCALE; yy+=SCALE) {
 			g.drawLine(0, yy, WIDTH*SCALE, yy);
+		}
+// draw the very snake		
+		for(int d = 0; d < s.length; d++) {
+			g.setColor(color(200, 130, 50));
+			g.fillRect(s.snakeX[d]*SCALE+1, s.snakeY[d]*SCALE+1, SCALE-1, SCALE-1);
 		}
 	}
 	
@@ -38,10 +56,19 @@ public class SnakeGame extends JPanel{
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setResizable(false);
+// size of the playing field
 		f.setSize(WIDTH*SCALE+7, HEIGHT*SCALE+30);
 		f.setLocationRelativeTo(null);
 		f.add(new SnakeGame());
 		f.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		s.move();
+		
+		repaint();
+		
 	}
 
 }
--- src/snake/objaects/Snake.java
+++ src/snake/objaects/Snake.java
@@ -1,5 +1,31 @@
 package snake.objaects;
 
 public class Snake {
+// direction of motion of the snake
+	public int direction = 0;
+// length of snake
+	public int length = 2;
+// the maximum number of elements in a snake
+	public int snakeX[] = new int[100];
+	public int snakeY[] = new int[100];
+// the initial location of our snake	
+	public Snake(int x0, int y0, int x1, int y1) {
+		snakeX[0] = x0;
+		snakeY[0] = y0;
+		snakeX[1] = x1;
+		snakeY[1] = y1;
+	}
 
+	public void move() {
+		// ???	
+		for(int d = length; d > 0; d--) {
+			snakeX[d] = snakeX[d-1];
+			snakeY[d] = snakeY[d-1];
+		}
+// The direction of movement of our snake depends on the arrow		
+		if (direction == 0) snakeY[0]--;
+		if (direction == 1) snakeX[0]++;
+		if (direction == 2) snakeY[0]++;
+		if (direction == 3) snakeX[0]--;
+	}
 }
